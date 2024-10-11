4860502ec540d43c3e49437b83d9fb13029f6ab6
andrewg5 <andrewge1911@gmail.com>
2024-10-10 17:02:41 -0700

    completed arrays activity

---
+++ Week 3/Arrays/Arrays.java
@@ -0,0 +1,9 @@
+public class Arrays {
+    public static void main(String[] args) {
+        String[] array = {"software", "Tomiko Itooka", "Up-A-Creek Robotics", "Destination: Deep Space"};
+
+        for(int i =0; i < 4; i++){
+            System.out.println(array[i]);
+        }
+    }
+}
